6365b6d1686a12e91e59a6d29a04c585c9f5e843
Paul Martin <paulmartinblopez@gmail.com>
2023-05-23 09:52:57 +0800

    Add prompting user for letter w/ invalid handling

--- lib/main.rb
+++ lib/main.rb
@@ -6,13 +6,26 @@ class HangMan
 
   def start
     word_to_guess = @dictionary[rand(@dictionary.length)]
+    current_guess = prompt_player_guess
     puts word_to_guess
+    puts current_guess
   end
 
   private
 
   def filter_words(word_list)
     word_list.keep_if { |word| word.chomp.length.between?(5, 12) }
+  end
+
+  def prompt_player_guess
+    print 'Enter a letter: '
+    guess = gets.chomp
+    until guess.match?(/\A[a-zA-Z]\Z/)
+      puts "\nIt seems like you gave an invalid input. Previous input: #{guess}"
+      print 'Enter a letter: '
+      guess = gets.chomp
+    end
+    guess
   end
 end
 
